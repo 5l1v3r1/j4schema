abf0dc4341ba09b603eadddb3bd1f8e93f904e47
Davide Tampellini <tampe125@gmail.com>
2012-04-21 00:44:30 +0200

    Started workin on Joomla integration

---
+++ extension/plugins/j4schema_jintegration/j4sjintegration.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package 	J4Schema
+ * @category	J4SchemaPro
+ * @copyright 	Copyright (c)2011 Davide Tampellini
+ * @license 	GNU General Public License version 3, or later
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+jimport('joomla.plugin.plugin');
+
+class plgSystemJ4sjintegration extends JPlugin
+{
+	public function onAfterRender()
+	{
+
+	}
+}
