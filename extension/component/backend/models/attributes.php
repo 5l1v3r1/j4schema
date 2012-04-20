50efba10ceec033d095cd2ff2b766843023c9dd3
Davide Tampellini <tampe125@gmail.com>
2012-04-20 20:49:37 +0200

    Added copyrights headers

--- extension/component/backend/models/attributes.php
+++ extension/component/backend/models/attributes.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package 	J4Schema
+ * @copyright 	Copyright (c)2011 Davide Tampellini
+ * @license 	GNU General Public License version 3, or later
+ * @since 		1.0
+ */
+
 defined('_JEXEC') or die();
 
 class J4schemaModelAttributes extends FOFModel
--- extension/component/backend/views/editor/tmpl/default.php
+++ extension/component/backend/views/editor/tmpl/default.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package 	Joomla4Schema
+ * @package 	J4Schema
  * @copyright 	Copyright (c)2011 Davide Tampellini
  * @license 	GNU General Public License version 3, or later
  * @since 		1.0
