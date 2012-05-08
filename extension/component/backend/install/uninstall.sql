e584879932d2231be8abc2b1e183ef694358cdc8
Davide Tampellini <tampe125@gmail.com>
2012-05-08 22:29:32 +0200

    Updated uninstall sql

--- extension/component/backend/install/uninstall.sql
+++ extension/component/backend/install/uninstall.sql
@@ -3,3 +3,4 @@ DROP TABLE IF EXISTS `#__j4schema_prop_values`;
 DROP TABLE IF EXISTS `#__j4schema_types`;
 DROP TABLE IF EXISTS `#__j4schema_type_prop`;
 DROP TABLE IF EXISTS `#__j4schema_tokens`;
+DROP TABLE IF EXISTS `#__j4schema_authors`;
