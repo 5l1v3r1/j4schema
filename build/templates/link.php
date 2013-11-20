5fb86ab46e2cfc71b94bf2ccbe977810dbffcf03
Davide Tampellini <tampe125@gmail.com>
2013-11-20 21:52:08 +0100

    Added support for ping build

---
+++ build/templates/link.php
@@ -0,0 +1,9 @@
+<?php
+
+$symlink_folders = array(
+	# Build files
+	'../buildfiles/bin'							=> 'build/bin',
+	'../buildfiles/buildlang'					=> 'build/buildlang',
+	'../buildfiles/phingext'					=> 'build/phingext',
+	'../buildfiles/tools'						=> 'build/tools',
+);
